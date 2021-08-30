7fea8d002e45772eb23557745d7b175919c9dd54
Satoshi Onoda <satoshionoda@gmail.com>
2021-08-30 16:28:10 +0900

    added strip unit

--- src/css/strip-px.ts
+++
@@ -1,5 +0,0 @@
-export const stripPx = (value: string): number | undefined => {
-  const reg = /(.*)px/.exec(value);
-  const result: any = !!reg ? reg[1] : value;
-  return isNaN(result) || !value ? undefined : parseInt(result);
-};
