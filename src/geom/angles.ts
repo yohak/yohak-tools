44d4e2fd4f5d406d72967df644561c8746a500cd
Satoshi Onoda <satoshionoda@gmail.com>
2021-11-10 15:07:42 +0900

    角度変換追加

---
+++ src/geom/angles.ts
@@ -0,0 +1,6 @@
+export const degreeToRadian = (deg: number): number => {
+  return (deg * Math.PI) / 180;
+};
+export const radianToDegree = (rad: number): number => {
+  return (rad / Math.PI) * 180;
+};
