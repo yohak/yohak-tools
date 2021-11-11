ad587d3d311effc8625b208442cb4b7dfedaa545
Satoshi Onoda <satoshionoda@gmail.com>
2021-11-11 17:56:43 +0900

    valueBetween追加

---
+++ src/geom/value-between.spec.ts
@@ -0,0 +1,28 @@
+import { valueBetween } from "./value-between";
+
+describe("valueBetween", () => {
+  it("should work", () => {
+    const result = valueBetween(100, 200, 0.5);
+    expect(result).toBe(150);
+  });
+  it("should work with reverse order", () => {
+    const result = valueBetween(200, 100, 0.5);
+    expect(result).toBe(150);
+  });
+  it("should work with overshoot value", () => {
+    const result = valueBetween(100, 200, 2);
+    expect(result).toBe(300);
+  });
+  it("should work with negative overshoot value", () => {
+    const result = valueBetween(100, 200, -1);
+    expect(result).toBe(0);
+  });
+  it("should camp overshoot value", () => {
+    const result = valueBetween(100, 200, 2, true);
+    expect(result).toBe(200);
+  });
+  it("should camp negative overshoot value", () => {
+    const result = valueBetween(100, 200, -1, true);
+    expect(result).toBe(100);
+  });
+});
---
+++ src/geom/value-between.ts
@@ -0,0 +1,12 @@
+export const valueBetween = (
+  a: number,
+  b: number,
+  v: number,
+  clamp: boolean = false
+): number => {
+  if (clamp) {
+    if (v > 1) v = 1;
+    if (v < 0) v = 0;
+  }
+  return a + (b - a) * v;
+};
