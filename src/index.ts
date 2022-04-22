a26913d6d627e5a0cd8dffe79a3d36145b024fd0
satoshionoda <satoshionoda@gmail.com>
2022-04-22 13:30:06 +0900

    Emotion関連を削除
    yohak-react-toolsを別に作る

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,5 @@ export * from "./layout/get-offset-from-document";
 export * from "./layout/get-offset-from-viewport";
 //
 export * from "./util/is";
-export * from "./util/Emotion";
 export * from "./util/promise";
 //
