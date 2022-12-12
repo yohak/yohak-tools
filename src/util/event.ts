ad578560e0fb9dcc2b2b5054b79af718286d7781
satoshionoda <satoshionoda@gmail.com>
2022-12-12 19:06:27 +0900

    event 追記
    resizeCompleteなど

--- src/util/event.ts
+++ src/util/event.ts
@@ -5,3 +5,26 @@ export const setOnReady = (handler: () => void) => {
 export const setOnLoad = (handler: () => void) => {
   window.addEventListener("load", handler);
 };
+
+export const setOnResize = (handler: () => void) => {
+  window.addEventListener("resize", handler);
+};
+
+export const setOnScroll = (handler: () => void) => {
+  document.addEventListener("scroll", handler);
+};
+
+export const setOnResizeComplete = (handler: () => void, completeMs = 166) => {
+  let timeout = 0;
+  window.addEventListener("resize", () => {
+    clearTimeout(timeout);
+    timeout = window.setTimeout(handler, completeMs);
+  });
+};
+export const setOnScrollComplete = (handler: () => void, completeMs = 166) => {
+  let timeout = 0;
+  document.addEventListener("scroll", () => {
+    clearTimeout(timeout);
+    timeout = window.setTimeout(handler, completeMs);
+  });
+};
